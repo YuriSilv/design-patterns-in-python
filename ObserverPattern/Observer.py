2d2e99320dc8089ccbd8bc868cfad330a1ee25e2
YuriSilv <yuridtna2002@hotmail.com>
2023-01-20 21:32:09 -0300

    obeserver stater

---
+++ ObserverPattern/Observer.py
@@ -0,0 +1,6 @@
+# "Observer Pattern defines a one-to-many dependency between objects so that when one object changes state, all its dependents are notified and updated automatically." - book Head First Design Patterns
+
+
+
+#Em minhas palavras: o padrão de projeto busca enviar um sinal de estado a outro objeto,
+#para dizer que ele mudou de estado e, assim, o outro objetivo pode agir de x forma.
