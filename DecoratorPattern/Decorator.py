0bcef55954941ae625410c28327ffdab95e15636
YuriSilv <yuridtna2002@hotmail.com>
2023-01-22 20:41:36 -0300

    decorator v.01

---
+++ DecoratorPattern/Decorator.py
@@ -0,0 +1,3 @@
+# "Decorator Pattern attaches additional responsibilities to an object dynamically. Decorators provide a flexible alternative to subclassing for extending functionality." - book Head First Design Patterns
+
+#strutural pattern
